da4d9e4ec38e9d86cc16eb7b7cbd8c1b47878594
Jauregui crespo <jauregui.crespo@gmail.com>
2021-12-30 13:48:37 -0300

    clean code

--- print-server.js
+++ print-server.js
@@ -1,88 +1,66 @@
-
 const ThermalPrinter = require("node-thermal-printer").printer;
 const Types = require("node-thermal-printer").types;
-const bodyParser = require('body-parser')
-
+const bodyParser = require("body-parser");
 
 async function printMsg(number) {
- 
-let printer = new ThermalPrinter({
-  type: Types.EPSON,
-  interface: '\\\\localhost\\epson'
-});
- 
-printer.alignCenter();
-  await printer.printImage('./assets/paris-logo.png');
+  let printer = new ThermalPrinter({
+    type: Types.EPSON,
+    interface: "\\\\localhost\\epson",
+  });
+
+  printer.alignCenter();
+  await printer.printImage("./assets/paris-logo.png");
   printer.newLine();
   printer.setTypeFontA();
-  printer.setTextSize(3,2);
-  // printer.println("Número");
-  // printer.newLine();
-  printer.setTextSize(2,2);
+  printer.setTextSize(3, 2);
+  printer.setTextSize(2, 2);
   printer.println(number);
-  
-printer.cut();
- 
-try {
-  console.error("Print done!");
-  return printer.execute()
-} catch (error) {
-  console.log("Print failed:", error);
-}
-   /*await printer.printDirect({
-        data: msg,
-        printer: 'epson',
-        type: 'RAW',
-        success: function (jobID) {
-            console.log(`printer job: ${jobID}`);
-            epsonThermalPrinter.clear();
-        },
-        error: function (err) {
-            console.log(err);
-        }
-    }) */
-  
-    // await printer.cut(); 
-   // printer.execute()s
-}
 
+  printer.cut();
 
-
-
-console.log('paso algo ')
-var express = require('express');
-var app = express();
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
-app.use((req,res,next)=>{
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With, x-access-token');
-  if ('OPTIONS' == req.method) {
-    res.sendStatus(200);
+  try {
+    console.error("Print done!");
+    return printer.execute();
+  } catch (error) {
+    console.log("Print failed:", error);
   }
-  else {
+}
+
+console.log("Conectado ");
+var express = require("express");
+var app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET,PUT,POST,DELETE,PATCH,OPTIONS"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, Content-Length, X-Requested-With, x-access-token"
+  );
+  if ("OPTIONS" == req.method) {
+    res.sendStatus(200);
+  } else {
     next();
   }
 });
-app.get('/', function(req, res) {
-  res.send('Hola Mundo!');
+app.get("/", function (req, res) {
+  res.send("Hola Mundo!");
 });
-app.post('/print', async function(req, res) {
-  const {number} =  req.body;
-  //printMsg(number)
+app.post("/print", async function (req, res) {
+  const { number } = req.body;
   const print_ = await printMsg(number);
-  console.log(print_)
+  console.log(print_);
+  res.status(200).send(req.body);
+});
 
-  res.status(200).send(req.body)
-})
-
-
-// Require the Routes API  
+// Require the Routes API
 // Create a Server and run it on the port 3000
 const server = app.listen(3000, function () {
-  let host = server.address().address
-  let port = server.address().port
+  let host = server.address().address;
+  let port = server.address().port;
   // Starting the Server at the port 3000
-})
-
+});
